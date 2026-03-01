a61124d965db49cc5149645ad214a030b5e615ad
Gadenaboi-Sandeep <gadenaboisandeep2@gmail.com>
2026-03-01 12:38:23 +0530

    Update base URL to Railway production backend

--- app/src/main/java/com/raithumitra/data/remote/RetrofitClient.java
+++ app/src/main/java/com/raithumitra/data/remote/RetrofitClient.java
@@ -8,7 +8,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://10.223.151.175:8080/"; // Phone Hotspot IP
+    private static final String BASE_URL = "https://raithumitra-growsmart-production.up.railway.app/"; // Live Railway
+                                                                                                       // backend
     private static Retrofit retrofit = null;
 
     public static ApiService getApiService(Context context) {
